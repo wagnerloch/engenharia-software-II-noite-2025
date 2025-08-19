d0b5c40d3f8d51c482eb9fd295525cefedc63c7c
Wagner Loch <wagner_loch@hotmail.com>
2025-08-19 20:46:17 -0300

    Add Fibonacci function

--- Main.js
+++ Main.js
@@ -1,7 +1,11 @@
 console.log("Hello, World!");
 
-
-
 for (let i = 0; i < 5; i++) {
     console.log(`Iteration: ${i}`);
+}
+
+function fibonacci(n) {
+    if (n < 0) return "Input should be a non-negative integer.";
+    if (n <= 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
