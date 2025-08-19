2c49eed2f762a2a09bfca70dcf209bfe03f9ec79
Wagner Loch <wagner_loch@hotmail.com>
2025-08-19 20:56:58 -0300

    Add message in Fibonacci

--- Main.js
+++ Main.js
@@ -5,6 +5,7 @@ for (let i = 0; i < 5; i++) {
 }
 
 function fibonacci(n) {
+    console.log("Calculating Fibonacci for:", n);
     if (n < 0) return "Input should be a non-negative integer.";
     if (n <= 1) return n;
     return fibonacci(n - 1) + fibonacci(n - 2);
