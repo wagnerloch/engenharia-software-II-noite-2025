19f28284d6bdd9b2dbab43a339332fb0cd94f9f4
Wagner Loch <wagner_loch@hotmail.com>
2025-08-19 21:04:14 -0300

    Add factorial function

--- Main.js
+++ Main.js
@@ -10,3 +10,15 @@ function fibonacci(n) {
     if (n <= 1) return n;
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+fibonacci(5);
+
+console.log("Fibonacci of 5:", fibonacci(5));
+
+function factorial(n) {
+    if (n < 0) return "Input should be a non-negative integer.";
+    if (n === 0) return 1;
+    return n * factorial(n - 1);
+}
+
+console.log("Factorial of 5:", factorial(5));
